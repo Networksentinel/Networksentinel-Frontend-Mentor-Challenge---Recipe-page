556a274b0ec13a9bfa4ade651bc9c11ba6f39fcc
Networksentinel <network.sentinel@protonmail.com>
2025-05-13 13:59:52 +0200

    docs/fix: fixed wrong vite.config setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ console.log("vite.config.js is loaded!");
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Frontend-Mentor-Challenge---Recipe-page/',
+  base: '/Networksentinel-Frontend-Mentor-Challenge---Recipe-page/',
 });
